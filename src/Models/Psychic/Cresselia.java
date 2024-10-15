b3405db820e321b724916a6e2f986908ac0c780a
S1mpotyaga <zfk.2024@gmail.com>
2024-10-15 17:32:59 +0300

    main1

--- src/Models/Psychic/Cresselia.java
+++ src/Models/Psychic/Cresselia.java
@@ -19,7 +19,7 @@ public class Cresselia extends Pokemon {
     public Cresselia(String name, int level) {
         super(name, level);
         setType(Type.PSYCHIC);
-        setStats(100, 70, 110, 75, 120, 85);
+        setStats(120, 70, 110, 75, 120, 85);
         setMove(new Psychic(), new ShadowBall(), new AuroraBeam(), new DreamEater());
     }
 }
